4056934c3f877df71aa28d1f1827d2d540c469d5
Michael Francis <michael.thomas.francis@gmail.com>
2024-10-25 17:10:49 -0400

    Updated css and removed about page

---
+++ spooktroop.js
@@ -0,0 +1,9 @@
+//Function to send submitted stories to database
+   // let story = user submitted story;
+    //story gets sent to database after submit button is clicked.
+    //a thank you message is displayed
+
+//Access submitted stories via an archive
+    //stories will be displayed by title in a container
+    //on click of title, the story gets displayed
+//
