8463fcd3ad93c461330450fb57ab0f2bd1d07028
Michael Francis <michael.thomas.francis@gmail.com>
2024-11-21 19:33:19 -0500

    Fixed server issue

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const path = require("path");
+const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
-mongoose.connect("mongodb+srv://michaelthomasfrancis:rjt6z0G7VVjzr0SP@cluster0.alu8q.mongodb.net/stories");
-
+mongoose.connect("mongodb+srv://michaelthomasfrancis:rjt6z0G7VVjzr0SP@cluster0.alu8q.mongodb.net/stories", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(err => {
+        console.logconsole.log("Error connecting to MongoDB", err);
+    });
+    
 //create a data schema
 const storiesSchema = {
     title: String,
     content: String
-}
+};
 
 const Story = mongoose.model("Story", storiesSchema);
 
-app.use(express.static(__dirname + '/public'));
-
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/submit.html")
-})
+    res.sendFile(path.join(__dirname, "/submit.html"))
+});
 
 app.post("/", function(req, res) {
     let newStory = new Story({
         title: req.body.title,
         content: req.body.content
     });
-    newStory.save();
-    res.redirect('/');
-})
+    newStory.save()
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            console.error("Error saving story", err);
+            res.status(500).send("There was an error saving your story.");
+        });
+    
+});
 
 app.listen(3000, function() {
     console.log("server is running on 3000");
-})
+});
