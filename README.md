59b5e6e868145397649846f5fdf32c55910a999a
Michael Francis <michael.thomas.francis@gmail.com>
2024-12-07 15:30:12 -0500

    Tidying up

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is an ongoing project that will be the capstone project for a class. This is aimed to be a 
 collection of anonymously submitted paranormal stories and keep an archive that can be read by
 whoever visits the site.
+
+
--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const path = require("path");
 const port = 3000;
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect("mongodb+srv://michaelthomasfrancis:rjt6z0G7VVjzr0SP@cluster0.alu8q.mongodb.net/stories", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to MongoDB");
     })
     .catch(err => {
-        console.logconsole.log("Error connecting to MongoDB", err);
+        console.log("Error connecting to MongoDB", err);
     });
     
 //create a data schema
@@ -23,10 +25,12 @@ const storiesSchema = {
 const Story = mongoose.model("Story", storiesSchema);
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/submit.html"))
+    res.sendFile(path.join(__dirname, "public"))
 });
 
+
 app.post("/", function(req, res) {
+
     let newStory = new Story({
         title: req.body.title,
         content: req.body.content
